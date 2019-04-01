8900d01a287dbdadf14bbd0268a05bdc6529107b
luisgrangeiro <luiscarlos.grangeiro@gmail.com>
2019-03-31 23:14:10 -0300

    :art: Melhora teste BoletoType Itau

--- itau/itau_test.go
+++ itau/itau_test.go
@@ -88,6 +88,23 @@ func TestRegiterBoletoItau(t *testing.T) {
 		So(output.BarCodeNumber, ShouldBeEmpty)
 		So(output.DigitableLine, ShouldBeEmpty)
 	})
+}
+
+func TestShouldMapItauBoletoType(t *testing.T) {
+	env.Config(true, true, true)
+	input := new(models.BoletoRequest)
+	if err := util.FromJSON(baseMockJSON, input); err != nil {
+		t.Fail()
+	}
+	bank := New()
+	go mock.Run("9097")
+	time.Sleep(2 * time.Second)
+
+	Convey("deve-se mapear corretamente o BoletoType quando informação for vazia", t, func() {
+		output := bank.GetBoletoType(input)
+		So(input.Title.BoletoType, ShouldEqual, "")
+		So(output, ShouldEqual, "01")
+	})
 
 	input.Title.BoletoType = "BP"
 	Convey("deve-se mapear corretamente o BoletoType de boleto de proposta", t, func() {
@@ -100,5 +117,4 @@ func TestRegiterBoletoItau(t *testing.T) {
 		output := bank.GetBoletoType(input)
 		So(output, ShouldEqual, "01")
 	})
-
 }
