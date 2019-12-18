dbbf99777c5838879a99c952a1178a1f54965352
Paulo Lima <paulolimarb@gmail.com>
2019-12-18 20:34:14 -0300

    add tag in all templates

--- boleto/templateBradescoShopFacil.go
+++ boleto/templateBradescoShopFacil.go
@@ -4,6 +4,8 @@ const templateBoletoBradescoShopFacil = `
 <html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+ 	<meta name="robots" content="noindex">
+
     <style>
 
         @media print
--- boleto/templatePefisa.go
+++ boleto/templatePefisa.go
@@ -4,6 +4,7 @@ const templateBoletoPefisa = `
 <html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta name="robots" content="noindex">
     <style>
 
         @media print
