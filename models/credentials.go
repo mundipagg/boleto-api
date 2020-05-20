e8a124944e3152352ab9248f8a2df3ea5e29c01a
Leonardo Jasmim <leonardo.jasmim@mundipagg.com>
2020-05-20 20:19:40 -0300

    :recycle: Update Credentials

--- models/credentials.go
+++ models/credentials.go
@@ -1,15 +1,19 @@
 package models
 
+import "gopkg.in/mgo.v2/bson"
+
 //Credentials Credenciais para requisição de Registro de Boleto
 type Credentials struct {
-	Username string `bson:"username"`
-	Password string `bson:"password"`
+	ID       bson.ObjectId `bson:"_id,omitempty"`
+	UserKey  string
+	Username string `bson:"username,omitempty"`
+	Password string `bson:"password,omitempty"`
 }
 
 //NewCredentials Cria uma instância de Credential
-func NewCredentials(u, p string) *Credentials {
+func NewCredentials(k, p string) *Credentials {
 	return &Credentials{
-		Username: u,
+		UserKey:  k,
 		Password: p,
 	}
 }
