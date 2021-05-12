699515383448a3d9489fbf35127feff285072747
Leonardo Jasmim <leonardo.jasmim@mundipagg.com>
2021-05-12 15:22:55 -0300

    :sparkles: Adiciona novos métodos e funcmaps

--- util/string.go
+++ util/string.go
@@ -39,3 +39,32 @@ func ParseJSON(s string, o interface{}) interface{} {
 	json.Unmarshal([]byte(s), o)
 	return o
 }
+
+//IsDigit Verifica se um caracter é um dígito numérico de acordo com o código decimal da Tabela ASCII,
+// onde o '0' representa o valor 48 e o '9' o valor 57
+func IsDigit(r rune) bool {
+	return (r >= 48 && r <= 57)
+}
+
+//IsBasicCharacter Verifica se um caracter é uma letra sem acento, maiúscula ou minúscula, de acordo com o código decimal da Tabela ASCII
+// onde o 'A' representa o valor 65 e o 'Z' o valor 90 e o 'a' representa o valor 97 e 'z' o valor 122
+//true para 0123456789
+func IsBasicCharacter(r rune) bool {
+	return (r >= 65 && r <= 90) || (r >= 97 && r <= 122)
+}
+
+//IsCaixaSpecialCharacter Verifica se um caracter especial é aceito Caixa Econômica, de acordo com o  código decimal da Tabela ASCII
+// sendo aceito os seguinte caracteres:
+//	esp	32	&	38	 :	58	 _  95
+//	!	33	'	39   ;	59
+//			(	40   <	60
+//			)	41   =	61
+//			*	42   >	62
+//			+	43   ?	63
+//			,	44
+//			-	45
+//			.	46
+//			/	47
+func IsCaixaSpecialCharacter(r rune) bool {
+	return (r >= 32 && r <= 33) || (r >= 38 && r <= 47) || (r >= 58 && r <= 63) || r == 95
+}
