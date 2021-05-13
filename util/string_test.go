2030a55619c8e8cb8673266e8078974cba45e6ec
Leonardo Jasmim <leonardo.jasmim@mundipagg.com>
2021-05-13 11:08:19 -0300

    :white_check_mark: Adiciona testes

--- util/string_test.go
+++ util/string_test.go
@@ -23,6 +23,21 @@ var padLeftParameters = []UtilTestParameter{
 	{Input: "1234567890", Expected: "1234567890"},
 }
 
+var digitParameters = []UtilTestParameter{
+	{Input: "0123456789", Expected: true},
+	{Input: " ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz,/()*&=-+!:?<>.;_\"", Expected: false},
+}
+
+var basicCharacter = []UtilTestParameter{
+	{Input: " 0123456789,/()*&=-+!:?<>.;_\"", Expected: false},
+	{Input: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz", Expected: true},
+}
+
+var caixaSpecialCharacter = []UtilTestParameter{
+	{Input: " ,/()*=-+!:?.;_'", Expected: true},
+	{Input: "01223456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz@#$%¨{}[]^~Çç\"&<>\\", Expected: false},
+}
+
 func TestRemoveDiacritics(t *testing.T) {
 	for _, fact := range removeDiacriticsParameters {
 		result := RemoveDiacritics(fact.Input.(string))
@@ -37,5 +52,35 @@ func TestPadLeft(t *testing.T) {
 	for _, fact := range padLeftParameters {
 		result := PadLeft(fact.Input.(string), paddingCaracter, uint(length))
 		assert.Equal(t, fact.Expected, result, "O numero deve ser ajustado corretamente")
+	}
+}
+
+func TestIsDigit(t *testing.T) {
+	for _, fact := range digitParameters {
+		s := fact.Input.(string)
+		for _, c := range s {
+			result := IsDigit(c)
+			assert.Equal(t, fact.Expected, result, "A verificação de dígito deve ocorrer corretamente")
+		}
+	}
+}
+
+func TestIsBasicCharacter(t *testing.T) {
+	for _, fact := range basicCharacter {
+		s := fact.Input.(string)
+		for _, c := range s {
+			result := IsBasicCharacter(c)
+			assert.Equal(t, fact.Expected, result, "A verificação de caracter deve ocorrer corretamente")
+		}
+	}
+}
+
+func TestIsSpecialCharacterCaixa(t *testing.T) {
+	for _, fact := range caixaSpecialCharacter {
+		s := fact.Input.(string)
+		for _, c := range s {
+			result := IsCaixaSpecialCharacter(c)
+			assert.Equal(t, fact.Expected, result, "A verificação de caracter deve ocorrer corretamente")
+		}
 	}
 }
